d014849ed67e7f824e1e2804f30540b7f29c797f
Marc Garreau <marcdgarreau@gmail.com>
2016-09-10 21:51:52 -0400

    Adds ace dual value functionality

--- hand.py
+++ hand.py
@@ -4,17 +4,27 @@ values = { 'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
 class Hand:
   def __init__(self):
     self.cards = []
+    self.ace = False
 
   def deal(self, deck):
     self.cards.append(deck.cards.pop())
     self.cards.append(deck.cards.pop())
+    self.check_for_ace()
 
   def hit(self, deck):
     self.cards.append(deck.cards.pop())
+    self.check_for_ace()
     print "Current hand: {cards}\n".format(cards=self.cards)
 
   def get_value(self):
     value = 0
     for card in self.cards:
       value += values[card[1:]]
+    if self.ace and value < 12:
+      value += 10
     return value
+
+  def check_for_ace(self):
+    for card in self.cards:
+      if card[1:] == 'A':
+        self.ace = True
