76fe86400c9d4f6e2b839ebf4d09a73b48746e8d
Marc Garreau <marcdgarreau@gmail.com>
2016-09-10 21:15:00 -0400

    Finishes refactoring Game class methods

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+from deck import Deck
+from hand import Hand
+
 values = { 'A': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
            '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10 }
 
@@ -6,16 +9,71 @@ class Game:
     self.chips = 100
 
   def play(self):
-    pass
+    print "\nA crappy blackjack game appears!"
+    while self.is_live():
+      deck = Deck()
+      player_hand = self.deal_hand(deck)
+      dealer_hand = self.deal_hand(deck)
+      bet = self.get_player_bet()
+      self.display_hands(player_hand, dealer_hand)
+      self.handle_user_actions(deck, bet, player_hand, dealer_hand)
+    else:
+      print "\nOut of chips!\nGame over!"
 
   def is_live(self):
     return self.chips > 0
 
-  def evaluate_hand(self, hand):
-    value = 0
-    for card in hand:
-      value += values[card[1:]]
-    return value
+  def deal_hand(self, deck):
+    hand = Hand()
+    deck.deal(hand)
+    return hand
+
+  def display_hands(self, player_hand, dealer_hand):
+    print "\nYour hand: {hand}".format(hand=self.display_player_hand(player_hand.cards))
+    print "Dealer's hand: {hand}\n".format(hand=self.display_dealer_hand(dealer_hand.cards))
+
+  def display_player_hand(self, hand):
+    return "{card_1}, {card_2}".format(card_1=hand[0], card_2=hand[1])
+
+  def display_dealer_hand(self, hand):
+    return "{card_1}, HIDDEN".format(card_1=hand[0])
+
+  def get_player_bet(self):
+    while True:
+      try:
+        print "\nYou have {chips} chips.".format(chips=self.chips)
+        bet = int(raw_input("How many would you like to bet? "))
+        if bet > self.chips:
+          print "You can't bet more than you have!"
+          continue
+        elif bet < 1:
+          print "Bet must be at least 1!"
+          continue
+        else:
+          return bet
+      except:
+        print "\nPlease enter a number of chips!"
+
+  def handle_user_actions(self, deck, bet, player_hand, dealer_hand):
+    while True:
+      input = raw_input('Want to (h)it or (s)tay? ')
+
+      if input == 'h':
+        print 'Hitting!'
+        deck.hit(player_hand)
+        if self.is_bust(player_hand):
+          print 'Bust!'
+          self.lose_chips(bet)
+          break
+      elif input == 's':
+        self.evaluate_round(bet, player_hand, dealer_hand)
+        break
+      else:
+        print "\nGotta enter 'h' or 's'!"
+        continue
+
+  def is_bust(self, hand):
+    return self.evaluate_hand(hand) > 21
 
   def evaluate_round(self, bet, player_hand, dealer_hand):
     player_points = self.evaluate_hand(player_hand)
@@ -29,6 +87,12 @@ class Game:
     else:
       print 'You lose!'
       self.lose_chips(bet)
+
+  def evaluate_hand(self, hand):
+    value = 0
+    for card in hand.cards:
+      value += values[card[1:]]
+    return value
 
   def win_chips(self, chips):
     self.chips += chips
